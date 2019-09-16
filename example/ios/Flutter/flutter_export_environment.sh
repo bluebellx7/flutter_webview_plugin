0321def238297a2a79048ad0e57a12b110c6dcff
zoushunfei <745423178@qq.com>
2019-09-16 13:56:59 +0800

    广告过滤功能

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=G:\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=G:\flutter\workspace\flutter_webview_plugin\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=G:\flutter\flutter\bin\cache\artifacts\engine\ios"
